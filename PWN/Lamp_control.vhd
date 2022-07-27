20d27c8985753fe6c20d57e8df66228ec15fd4a2
AlessandroDalpiaz <44235430+AlessandroDalpiaz@users.noreply.github.com>
2022-07-27 11:34:35 +0200

    Update Lamp_control.vhd
    Update

--- PWN/Lamp_control.vhd
+++ PWN/Lamp_control.vhd
@@ -1,61 +1,61 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC UNSIGNED.ALL;
-- Uncomment the following LIBRARY declaration IF using
+-- Uncomment the following LIBRARY declaration IF using
 -- arithmetic functions with Signed or Unsigned values 
 USE IEEE.NUMERICSTD.ALL;
 
-- Uncamment the following LIBRARY declaration IF instantiating
+-- Uncamment the following LIBRARY declaration IF instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-'ENTITY Lamp_control IS
+ENTITY Lamp_control IS
 PORT (
-cik : IN std logic;
-Lamptggiatore : IN std logic;
+clk : IN std_logic;
+Lampeggiatore : IN std_logic;
 Lamp_out : OUT STD_LOGIC
 );
 END Lamp_control;
 
 ARCHITECTURE Behavioral OF Lamp_control IS
 
-SIGNAL LampeggiatoreCounter : STD_LOGIC vector(26 DOWNTO 0) := std logic vector(to_unsigned(100000000, 27));
-SIGNAL LampeggiatoreStart : std logic vector(26 DOWNTO 0) := STD_LOGIC vector(to_unsigned(100000000, 27));
-SIGNAL LampSig : std logic := '1';
+SIGNAL LampeggiatoreCounter : STD_LOGIC_VECTOR(26 DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(100000000, 27));
+SIGNAL LampeggiatoreStart : STD_LOGIC_VECTOR(26 DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(100000000, 27));
+SIGNAL LampSig :STD_LOGIC := '1';
 
 TYPE Lamp_state_type IS (LampUp_O, LampUp_1, LampUp_2, LampUp_3, LampDown_0, LampDown_l, LampDown_2, LampDown_3);
 SIGNAL LampState : Lamp_state_type := LampUp_0;
 
 BEGIN
 
-LampProc : PROCESS (c1k)
+LampProc : PROCESS (clk)
 BEGIN
-  IF (rising_edge(c1k)) THEN
-    LampeggiatoreCounter <- LampeggiatoreCounter - '1';
-    IF (LampeggiatoreCounter = std logic vector(to_unsigned(0, 27))) THEN
+  IF (rising_edge(clk)) THEN
+    LampeggiatoreCounter <= LampeggiatoreCounter - '1';
+    IF (LampeggiatoreCounter = STD_LOGIC_VECTOR(to_unsigned(0, 27))) THEN
       LampeggiatoreCounter <= LampeggiatoreStart;
       LampSig <= NOT(LampSig);
     END IF;
   END IF;
 END PROCESS;
 
-LampControlProc : PROCESS (c1k)
+LampControlProc : PROCESS (clk)
 BEGIN
-  IF (rising_edge(c1k)) THEN
+  IF (rising_edge(clk)) THEN
 
     CASE (LampState) IS
 
       WHEN LampUp_O =>
 
         IF (Lampeggiatore = '1') THEN
-          LampeggiatoreStart ‹■ '0' t LampeggiatoreStart(26 DOWNTO 1);
+          LampeggiatoreStart <= '0' & LampeggiatoreStart(26 DOWNTO 1);
           LampState <= LampUp_1;
         ELSE
           LampState <= LampUp_0;
         END IF;
 
-      WHEN LampUp_l
+      WHEN LampUp_l =>
 
         IF (Lampeggiatore = '1') THEN
           LampeggiatoreStart <= '0' & LampeggiatoreStart(26 DOWNTO 1);
@@ -84,17 +84,17 @@ BEGIN
 
       WHEN LampDown_O =>
 
-        IF (Lampeggiatore • '!') THEN
-          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) c '0';
+        IF (Lampeggiatore = '1') THEN
+          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) & '0';
           LampState <= LampDown_l;
         ELSE
-          Lamp5tate <= LampDown_0;
+          LampState <= LampDown_0;
         END IF;
 
       WHEN LampDown_l =>
 
-        IF (Lampeggiatore = '!') THEN
-          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) s '0';
+        IF (Lampeggiatore = '1') THEN
+          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) & '0';
           LampState <= LampDown_2;
         ELSE
           LampState <= LampDown_l;
@@ -102,9 +102,9 @@ BEGIN
 
       WHEN LampDown_2 =>
 
-        IF (Lampeggiatore • '!') THEN
-          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) a '0';
-          Lamp5tate <= LampDown_3;
+        IF (Lampeggiatore = '1') THEN
+          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) & '0';
+          LampState <= LampDown_3;
         ELSE
           LampState <= LampDown_2;
         END IF;
@@ -112,7 +112,7 @@ BEGIN
       WHEN LampDown_3 =>
 
         IF (Lampeggiatore = '!') THEN
-          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) s '0';
+          LampeggiatoreStart <= LampeggiatoreStart(25 DOWNTO 0) & '0';
           LampState <= LampUp_0;
         ELSE
           LampState <= LampDown_3;
